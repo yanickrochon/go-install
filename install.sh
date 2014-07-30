f2d07a83775e46962d3a72bc344d4b709bfe9b85
Yanick Rochon <yanick.rochon@gmail.com>
2014-07-30 09:40:11 -0400

    Replaced sh with bash. Added checks when registering paths

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Go installation script
 #
@@ -14,7 +14,7 @@
 VERSION="1.3"
 
 # OS should be 'linux'
-OS=`uname`
+OS=`uname | tr '[:upper:]' '[:lower:]'`
 
 # Arch
 if [ `uname -i` = "x86_64" ]
@@ -23,6 +23,8 @@ then
 else
   ARCH="386"
 fi
+
+GO_REPO="http://golang.org/dl/"
 
 ARCHIVE_NAME="go$VERSION.$OS-$ARCH.tar.gz"
 INSTALL_PATH="/usr/local"
@@ -33,25 +35,52 @@ GO_BIN="$GO_PATH/bin"
 GO_PKG="$GO_PATH/pkg"
 GO_SRC="$GO_PATH/src"
 
+## taken from http://stackoverflow.com/questions/4686464/howto-show-wget-progress-bar-only/4687912?noredirect=1#comment38937490_4687912
+## ... bottom line is that read cause an error "Illegal option -d"
+progressfilt ()
+{
+    local flag=false c count cr=$'\r' nl=$'\n'
+    while IFS='' read  -d  '' -rn 1 c
+    do
+        if $flag
+        then
+            printf '%c' "$c"
+        else
+            if [[ $c != $cr && $c != $nl ]]
+            then
+                count=0
+            else
+                ((count++))
+                if ((count > 1))
+                then
+                    flag=true
+                fi
+            fi
+        fi
+    done
+}
 
 if ! [ -f $BIN_PATH/go ]
 then
 
   echo "Downloading Go $VERSION..."
-  wget -c --progress=bar:force http://golang.org/dl/$ARCHIVE_NAME
+  wget -c --progress=bar:force $GO_REPO$ARCHIVE_NAME 2>&1 | progressfilt
+  #wget -c --progress=bar:force $GO_REPO$ARCHIVE_NAME
 
   echo "Installing..."
   sudo tar -C /usr/local -xzf $ARCHIVE_NAME
 
-  # register binary path
-  if ! grep -Fq "$BIN_PATH" /etc/profile
-  then
-    echo "Registering path..."
-    echo "\nexport PATH=\$PATH:$BIN_PATH" | sudo tee --append /etc/profile > /dev/null
-    source /etc/profile
-  fi
-
   rm $ARCHIVE_NAME
+fi
+
+# register binary path
+if ! grep -Fq "$BIN_PATH" /etc/profile
+then
+  printf "\n" \
+         "if [ -d \"$BIN_PATH\" ]; then\n" \
+         "    PATH=\"\$PATH:$BIN_PATH\"\n" \
+         "fi" \
+         | sudo tee --append /etc/profile > /dev/null
 fi
 
 # create our project structure
@@ -62,8 +91,16 @@ mkdir -p $GO_SRC
 # register local path
 if ! grep -Fq "GOPATH=" $HOME/.profile
 then
-  echo "\nexport GOPATH=$GO_PATH\nexport PATH=\$PATH:$GO_BIN" >> $HOME/.profile
-  source "$HOME/.profile"
+  printf "\n" \
+         "if [ -n \"$GO_PATH\" ]; then\n" \
+         "    export GOPATH=\"$GO_PATH\"\n" \
+         "fi\n" \
+         "if [[ $PATH != *$GO_BIN* ]; then\n" \
+         "    export PATH=\"\$PATH:$GO_BIN\"\n" \
+         "fi" >> $HOME/.profile
 fi
+
+source /etc/profile
+source $HOME/.profile
 
 echo
